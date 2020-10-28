15841f66b380e649c234374fd536e63fbe3ba3b5
cristianiniguez <felizpc1@gmail.com>
2020-10-28 18:13:06 -0400

    matchers: monitoreo

--- __tests__/matchers.test.js
+++ __tests__/matchers.test.js
@@ -3,12 +3,16 @@ describe('Comparadores comunes', () => {
     name: 'Cristian',
     lastname: 'Iñiguez',
   };
+  const user1 = {
+    name: 'Cristian',
+    lastname: 'Iñiguez',
+  };
   const user2 = {
     name: 'Mary',
     lastname: 'Saucedo',
   };
   test('igualdad de elementos', () => {
-    expect(user).toEqual(user2);
+    expect(user).toEqual(user1);
   });
   test('desigualdad de elementos', () => {
     expect(user).not.toEqual(user2);
