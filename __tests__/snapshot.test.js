7faf7591e344099cd8754fe4685aa4415e2b929b
cristianiniguez <felizpc1@gmail.com>
2020-10-30 12:09:36 -0400

    snapshot testing: excepciones

--- __tests__/snapshot.test.js
+++ __tests__/snapshot.test.js
@@ -5,4 +5,20 @@ describe('Es hora de las instantaneas', () => {
   test('Snapshot', () => {
     expect(getCharacter(rick)).toMatchSnapshot();
   });
+  test('Siempre fallara la instantanea', () => {
+    const user = {
+      createdAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+    };
+    expect(user).toMatchSnapshot();
+  });
+  test('Tenemos una excepción', () => {
+    const user = {
+      id: Math.floor(Math.random() * 20),
+      name: 'Cristian Iñiguez',
+    };
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number),
+    });
+  });
 });
